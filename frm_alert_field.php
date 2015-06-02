f2e482ab02ffc214d0dd9157f6dc1fba649c0165
Tobias Järvelöv <tobias@jarvelov.se>
2015-06-02 12:58:22 +0200

    Update

--- frm_alert_field.php
+++ frm_alert_field.php
@@ -165,7 +165,7 @@ class Frm_Alert_Field Extends Frm_Alert {
             $form_id = intval($field['form_id']);
             $trigger_fields = Frm_Alert_Field::get_form_field_names_and_values($form_id);
 
-            //var_dump($trigger_fields);
+            var_dump($trigger_fields);
             
             ?>
                 <tr><td><label>Field Size</label></td>
