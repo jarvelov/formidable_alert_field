dee48388de1617745ac55265a9a2861e5281b2d5
Tobias Järvelöv <tobias@jarvelov.se>
2015-06-02 11:50:01 +0200

    Update

--- frm_alert_field.php
+++ frm_alert_field.php
@@ -85,9 +85,10 @@ class Frm_Alert_Field Extends Frm_Alert {
               }
             }
 
+            $form_id = intval($field['form_id']);
             $form_fields = FrmField::get_all_for_form($form_id);
 
-            var_dump($field);
+
             
             ?>
                 <tr><td><label>Field Size</label></td>
