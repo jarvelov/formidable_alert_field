5c8e0ae2122b02a9e3da3a0c9e2d755196ec9e94
Tobias Järvelöv <tobias@jarvelov.se>
2015-06-04 11:29:46 +0200

    Update

--- js/frm_alert_field.js
+++ js/frm_alert_field.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($){
     $('.trigger_fields_select').change(function() {
         $('.alert_trigger_fields_container').hide(); //hide all trigger fields initially
-        $('#alert_trigger_value_option_' + this.value).show(); //show the appropriate trigger field's values
+        $('#alert_trigger_value_option_' + this.value).css( "display", "inline"); //show the appropriate trigger field's values
     });
 });
