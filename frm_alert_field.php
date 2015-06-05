0c14c355242487b293dd1cf7a1e0ce1c846f72ef
Tobias Järvelöv <tobias@jarvelov.se>
2015-06-05 21:00:36 +0200

    Update

--- frm_alert_field.php
+++ frm_alert_field.php
@@ -28,7 +28,7 @@ class Frm_Alert_Field Extends Frm_Alert {
         add_action('frm_field_options_form', array($this, 'alert_field_options'), 10, 3);
 
         //Set field option values
-        add_filter( 'frm_setup_edit_field_vars', 'alert_field_options_values', 30 );
+        add_filter( 'frm_setup_edit_field_vars', array($this, 'alert_field_options_values'), 30 );
 
         //Show field in the front end
         add_action('frm_form_fields', array($this, 'alert_field_front_end'), 10, 2);
