09ce8199bed30249e710d4535085a07ea827272b
Tobias Järvelöv <tobias@jarvelov.se>
2015-06-06 15:28:47 +0200

    Update

--- frm_alert_field.php
+++ frm_alert_field.php
@@ -79,8 +79,7 @@ class Frm_Alert_Field Extends Frm_Alert {
 
     //Set values for each field or fall back to the default value
     public function alert_field_options_values( $values ) {
-        var_dump($values);
-        break;
+        var_dump('STALIN');
         $defaults = $this->get_alert_field_defaults();
 
         foreach ( $defaults as $option => $default_value ) {
