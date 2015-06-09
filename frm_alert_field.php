76dbb597fa43f229465e85831865ea6425be6182
Tobias Järvelöv <tobias@jarvelov.se>
2015-06-09 14:55:23 +0200

    Update

--- frm_alert_field.php
+++ frm_alert_field.php
@@ -176,7 +176,7 @@ class Frm_Alert_Field Extends Frm_Alert {
             <tr><td><label>Alert Condition</label></td>
                 <td>
                 <?php
-                var_dump( ($field['trigger_fields_select']) );
+                var_dump( $field );
                     $trigger_field = '<select name="field_options[trigger_fields_select_' . $field['id'] . '" class="trigger_fields_select">';
                     $trigger_field .= '<option value="">— Select —</option>';
                     $trigger_values = NULL;
