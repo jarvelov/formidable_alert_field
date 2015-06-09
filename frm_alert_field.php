2d31920b6f9c173d5f5603faaba96a08a63de54c
Tobias Järvelöv <tobias@jarvelov.se>
2015-06-09 16:03:45 +0200

    Update

--- frm_alert_field.php
+++ frm_alert_field.php
@@ -204,7 +204,7 @@ class Frm_Alert_Field Extends Frm_Alert {
 
                 $trigger_values .= '<input type="text" name="field_options[alert_trigger_value_custom_value_' . $field['id']  . ']" placeholder="Enter a custom value" value="' . esc_attr($field['alert_trigger_value_custom_value']) . '" id="alert_trigger_value_custom_value_' . $key . '" class="alert_trigger_value_custom_value" />';
             } else {
-                $trigger_values .= '<input type="text" name="field_options[alert_trigger_value_' . $field['id'] . ']" value="' . $value['value'] . '" id="alert_trigger_value_' . $key . '" />';
+                $trigger_values .= '<input type="text" name="field_options[alert_trigger_value_' . $field['id'] . ']" value="' . esc_attr($field['alert_trigger_value']) . '" id="alert_trigger_value_' . $key . '" />';
             }
 
             $trigger_values .= '</div>'; // ./alert_trigger_fields_container
