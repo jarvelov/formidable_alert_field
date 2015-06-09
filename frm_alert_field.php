cf2fcab07ba7da11e98285e281cfb20cab8e8aed
Tobias Järvelöv <tobias@jarvelov.se>
2015-06-09 16:20:29 +0200

    Update

--- frm_alert_field.php
+++ frm_alert_field.php
@@ -157,33 +157,16 @@ class Frm_Alert_Field Extends Frm_Alert {
         return $field_options;
     }
 
-    /** alert_field_options
-    * Add options to configure field in form builder
-    * Hook for: frm_field_options_form
+    /** get_alert_condition_fields
+    *
     */
-    public function alert_field_options($field, $display, $values){
-        if ( $field['type'] != 'frm_alert_field' ) {
-          return;
-        }
-
+    private function get_alert_condition_fields($field, $form_fields) {
         $defaults = $this->get_alert_field_defaults();
-
-        foreach($defaults as $key => $value) {
-            if( !isset($field[$key])) {
-                $field[$key] = $value;
-            }
-        }
-
-        //Get all fields in form to build trigger alert option
-        $form_id = intval($field['form_id']);
-        $trigger_fields = $this->get_form_field_names_and_values($form_id);
-
-        $html = '<tr><td><label>Alert Condition</label></td><td>';
         $trigger_field = '<select name="field_options[trigger_fields_select_' . $field['id'] . '" class="trigger_fields_select">';
         $trigger_field .= '<option value="">— Select —</option>';
         $trigger_values = NULL;
         //trigger_fields
-        foreach ($trigger_fields as $key => $value) {
+        foreach ($form_fields as $key => $value) {
             $trigger_field_selected = selected($field['trigger_fields_select'], $key, false );
             $trigger_field .= '<option value="' . $key . '" ' . $trigger_field_selected . '>' . $value['name'] . '</option>';
             $trigger_value_show = ($trigger_field_selected) ? 'active_value' : 'inactive_value';
@@ -220,7 +203,7 @@ class Frm_Alert_Field Extends Frm_Alert {
         }
         $trigger_operator .= '</select>';
 
-        $html .= '<div class="alert_settings_container">';
+        $html = '<div class="alert_settings_container">';
 
         $html .= '<div class="alert_trigger_field_container">';
         $html .= $trigger_field;
@@ -235,6 +218,82 @@ class Frm_Alert_Field Extends Frm_Alert {
         $html .= '</div>'; // ./alert_trigger_value_container
 
         $html .= '</div>'; // ./alert_settings_container
+
+        return $html;
+    }
+    /** get_alert_condition_fields
+    *
+    */
+    private function get_alert_condition_fields($field) {
+        $defaults = $this->get_alert_field_defaults();
+
+        //What action to perform when conditions are met
+        $trigger_action = '<select name="field_options[trigger_field_action_' . $field['id'] . ']" class="trigger_field_action">';
+        $trigger_action .= '<option value="">— Select —</option>';
+        foreach ($defaults['actions'] as $key => $value) {
+            $trigger_action .= '<option value="' . $key . '">' . $value . '</option>';
+        }
+        $trigger_action .= '</select>';
+
+        //Actions
+        $alert_action_fields = '<div class="alert_action_field" id="alert_action_email">';
+        $alert_action_fields .= '<input type="email" name="field_options[alert_action_email_' . $field['id'] . ' class="alert_actions" id="alert_action_email" placeholder="Ex. [admin_email] or [125]" />';
+        $alert_action_fields .= '</div>'; // ./alert_action_field
+
+        $alert_action_fields .= '<div class="alert_action_field" id="alert_action_frm_action">';
+        $alert_action_fields .= '<input type="number" name="field_options[alert_action_frm_action_' . $field['id'] . ' class="alert_actions" id="alert_action_frm_action" placeholder="Formidable ID, ex. 1388" />';
+        $alert_action_fields .= '</div>'; // ./alert_action_field
+
+        $alert_action_fields .= '<div class="alert_action_field" id="alert_action_update_field_value">';
+        $alert_action_fields .= '<input type="text" name="field_options[alert_action_update_field_value_' . $field['id'] . ']" class="alert_actions" id="alert_action_update_field_value" disabled="disabled" value="Not working atm...">';
+        $alert_action_fields .= '</div>'; // ./alert_action_field
+
+        $html = '<div class="alert_actions_container">';
+
+        $html .= '<div class="alert_trigger_action_container">';
+        $html .=  $trigger_action;
+        $html .= '</div>'; // ./alert_trigger_action_container
+
+        $html .= '<div class="alert_action_fields_container">';
+        $html .= $alert_action_fields;
+        $html .= '</div>'; // ./alert_action_fields_container
+
+        $html .= '</div>'; // ./alert_actions_container
+
+        return $html;
+    }
+
+
+    /** alert_field_options
+    * Add options to configure field in form builder
+    * Hook for: frm_field_options_form
+    */
+    public function alert_field_options($field, $display, $values){
+        if ( $field['type'] != 'frm_alert_field' ) {
+          return;
+        }
+
+        $defaults = $this->get_alert_field_defaults();
+
+        foreach($defaults as $key => $value) {
+            if( !isset($field[$key])) {
+                $field[$key] = $value;
+            }
+        }
+
+        //Get all fields in form to build trigger alert option
+        $form_id = intval($field['form_id']);
+        $form_fields = $this->get_form_field_names_and_values($form_id);
+
+        $html = '<tr><td><label>Alert Condition</label></td><td>';
+
+        $html .= $this->get_alert_condition_fields($field, $form_fields);
+
+        $html .= '</td></tr>';
+
+        $html .= '<tr><td><label>Alert Action</label></td><td>';
+
+        $html .= $this->get_alert_action_fields($field);
 
         $html .= '</td></tr>';
 
