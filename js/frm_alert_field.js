f4e51b2dd500a300594558618417dcc8640c4f1c
Tobias Järvelöv <tobias@jarvelov.se>
2015-06-11 17:39:02 +0200

    Update

--- js/frm_alert_field.js
+++ js/frm_alert_field.js
@@ -1,10 +1,12 @@
 jQuery(document).ready(function($){
     $(document).on("change",".trigger_fields_select",function() {
+        console.log(this.value);
         $('.alert_trigger_fields_container').hide(); //hide all trigger fields initially
         $('#alert_trigger_value_option_' + this.value).css( "display", "inline"); //show the appropriate trigger field's values
     });
 
     $(document).on("change",".alert_trigger_value_select",function() {
+        console.log(this.value);
         if(this.value == 'custom_value') {
             id = this.id.substr( this.id.lastIndexOf('_') +1 ); //get id number of select element to know which custom_value field to show
             $('#alert_trigger_value_custom_value_' + id).show(); //show relevant custom_value field
@@ -21,6 +23,7 @@ jQuery(document).ready(function($){
     });
 
     $(document).on("change","#alert_delay_active",function() {
+        console.log(this.value);
       if(this.checked) {
         $('.alert_setting').show();
       } else {
