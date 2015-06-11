50ec739828684b969fd134a124dca91a25537040
Tobias Järvelöv <tobias@jarvelov.se>
2015-06-11 17:34:41 +0200

    Make event listener work for dynamic elements

--- js/frm_alert_field.js
+++ js/frm_alert_field.js
@@ -1,10 +1,10 @@
 jQuery(document).ready(function($){
-    $('.trigger_fields_select').change(function() {
+    $(document).on("change",".trigger_fields_select",function() {
         $('.alert_trigger_fields_container').hide(); //hide all trigger fields initially
         $('#alert_trigger_value_option_' + this.value).css( "display", "inline"); //show the appropriate trigger field's values
     });
 
-    $('.alert_trigger_value_select').change(function() {
+    $(document).on("change",".alert_trigger_value_select",function() {
         if(this.value == 'custom_value') {
             id = this.id.substr( this.id.lastIndexOf('_') +1 ); //get id number of select element to know which custom_value field to show
             $('#alert_trigger_value_custom_value_' + id).show(); //show relevant custom_value field
@@ -13,14 +13,14 @@ jQuery(document).ready(function($){
         }
     });
 
-    $('.trigger_field_action').change(function() {
+    $(document).on("change",".trigger_field_action",function() {
       console.log(this.value);
         $('.alert_action_field').hide();
 
         $('#alert_action_' + this.value).css( "display", "inline"); //show the appropriate trigger field's values
     });
 
-    $('#alert_delay_active').change(function() {
+    $(document).on("change","#alert_delay_active",function() {
       if(this.checked) {
         $('.alert_setting').show();
       } else {
